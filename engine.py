99322c4becba784ca4f61385e1c9b12b20626605
Code Dragon <Kehvarl@gmail.com>
2017-07-01 23:02:06 -0700

    Working on Part 2. Created Entity object Modified Engine to use Entity for Player. NPC not yet displayed.

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import libtcodpy as libtcod
+from entity import  Entity
 import input_handler
 
 
@@ -6,8 +7,12 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    center_x = int(screen_width / 2)
+    center_y = int(screen_height / 2)
+    player = Entity(center_x, center_y, '@', libtcod.white)
+    npc = Entity(center_x-5, center_y, '@', libtcod.yellow)
+
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -22,11 +27,11 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
         libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_blit(con, 0,0, screen_width, screen_height, 0,0,0)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
 
         action = input_handler.handle_keys(key)
         move = action.get('move')
@@ -35,8 +40,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
