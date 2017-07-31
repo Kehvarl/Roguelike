3ee1bde2b50ce9aabfe28e810c4094d08815aedc
Code Dragon <Kehvarl@gmail.com>
2017-07-30 17:10:21 -0700

    Implemented scrolls and the Lightning spell

--- item_functions.py
+++ item_functions.py
@@ -4,6 +4,11 @@ from game_messages import Message
 
 
 def heal(*args, **kwargs):
+    """
+
+    :param args:
+    :param kwargs:
+    """
     entity = args[0]
     amount = kwargs.get('amount')
 
@@ -14,5 +19,42 @@ def heal(*args, **kwargs):
     else:
         entity.fighter.heal(amount)
         results.append({'consumed': True, 'message': Message('Your wounds start to feel better', libtcod.green)})
+
+    return results
+
+
+def cast_lightning(*args, **kwargs):
+    """
+
+    :param args:
+    :param kwargs:
+    """
+    caster = args[0]
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+
+    results = []
+
+    target = None
+    closest_distance = maximum_range + 1
+
+    for entity in entities:
+        if entity.fighter and entity != caster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+            distance = caster.distance_to(entity)
+
+            if distance < closest_distance:
+                target = entity
+                closest_distance = distance
+
+    if target:
+        results.append({'consumed': True, 'target': target,
+                        'message': Message(
+                            'A lightning bolt strikes the {0} with a loud thunder, dealing {1} damage'.format(
+                                target.name, damage),
+                            libtcod.white)})
+    else:
+        results.append({'consumed': False, 'message': Message('No enemy is close enough to strike', libtcod.red)})
 
     return results
