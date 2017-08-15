0d7660a87fbf85881d57fd3bc15f339a4c6277f7
Code Dragon <Kehvarl@gmail.com>
2017-08-14 21:20:29 -0700

    Modified random selectors. Dungeon gets harder as you descend

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -38,9 +38,6 @@ def get_constants():
     fov_light_walls = True
     fov_radius = 10
 
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
     # Usable Colors
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
@@ -68,8 +65,6 @@ def get_constants():
         'fov_algorithm': fov_algorithm,
         'fov_light_walls': fov_light_walls,
         'fov_radius': fov_radius,
-        'max_monsters_per_room': max_monsters_per_room,
-        'max_items_per_room': max_items_per_room,
         'colors': colors
     }
 
@@ -80,7 +75,7 @@ def get_game_variables(constants):
     # Initial Player and NPC locations
     # center_x = int(screen_width / 2)
     # center_y = int(screen_height / 2)
-    fighter_component = Fighter(hp=30, defense=2, power=5)
+    fighter_component = Fighter(hp=100, defense=1, power=4)
     inventory_component = Inventory(26)
     level_component = Level()
     player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
@@ -90,8 +85,7 @@ def get_game_variables(constants):
     # Generate Game Map
     game_map = GameMap(constants['map_width'], constants['map_height'])
     game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'],
-                      constants['map_width'], constants['map_height'], player, entities,
-                      constants['max_monsters_per_room'], constants['max_items_per_room'])
+                      constants['map_width'], constants['map_height'], player, entities)
 
     message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
