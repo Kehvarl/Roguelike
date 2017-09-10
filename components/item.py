3c09c5f265c193d5efdcb9ff0908766578247865
Code Dragon <Kehvarl@gmail.com>
2017-09-10 12:09:53 -0700

    Treasure -- Initial Commit.   Items have a Treasure Value   Entities have a Treasure Value   Upon Collecting an Item, its TV is set to 0 and the Collector's TV is increased by the Item's TV   A dropped Item has no TV by default   Upon death, a monster drops an Item with all it's TV

--- components/item.py
+++ components/item.py
@@ -1,6 +1,8 @@
 class Item:
-    def __init__(self, use_function=None, targeting=False, targeting_message=None, **kwargs):
+    def __init__(self, use_function=None, targeting=False, targeting_message=None,
+                 treasure_value=None, **kwargs):
         self.use_function = use_function
         self.targeting = targeting
         self.targeting_message = targeting_message
+        self.treasure_value = treasure_value
         self.function_kwargs = kwargs
