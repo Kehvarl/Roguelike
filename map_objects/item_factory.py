57d1da692b2cff0dc7493bdc6542f6308344a3ec
Code Dragon <Kehvarl@gmail.com>
2017-09-16 09:54:08 -0700

    Factories can now generate monsters by name from the provided dict, or custom monsters when provided necessary parameters.

--- map_objects/item_factory.py
+++ map_objects/item_factory.py
@@ -10,64 +10,123 @@ from render_functions import RenderOrder
 
 
 class ItemFactory:
+    use_functions = {'heal': heal,
+                     'cast_lightning': cast_lightning,
+                     'cast_confuse': cast_confuse,
+                     'cast_fireball': cast_fireball,
+                     None: None
+                     }
+    slots = {'main_hand': EquipmentSlots.MAIN_HAND,
+             'off_hand': EquipmentSlots.OFF_HAND,
+             None: None}
+
     @staticmethod
     def get_item(item_dict, item_chances, entities, x, y):
-        use_functions = {'heal': heal,
-                         'cast_lightning': cast_lightning,
-                         'cast_confuse': cast_confuse,
-                         'cast_fireball': cast_fireball,
-                         None: None
-                         }
-        slots = {'main_hand': EquipmentSlots.MAIN_HAND,
-                 'off_hand': EquipmentSlots.OFF_HAND,
-                 None:None}
-
         if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-            item_choice = item_dict[random_choice_from_dict(item_chances)]
+            return ItemFactory.get_item_by_name(item_dict,
+                                                random_choice_from_dict(item_chances),
+                                                x, y)
 
-            color = getattr(libtcod, item_choice['color'], libtcod.white)
-            item_component = None
-            equippable_component = None
-            targeting_message = None
-            treasure_value = item_choice.get('treasure_value', 0)
+    @staticmethod
+    def get_item_by_name(item_dict, item_name, x, y):
+        item_choice = item_dict[item_name]
 
-            if item_choice.get('item'):
-                use_function = use_functions.get(item_choice['item_parameters'].get('function'))
-                amount = item_choice['item_parameters'].get('amount')
-                targeting = item_choice['item_parameters'].get('targeting')
-                is_treasure = item_choice['item_parameters'].get('is_treasure', False)
-                message = item_choice['item_parameters'].get('message')
-                if message:
-                    message = Message(message,
-                                      item_choice['item_parameters'].get('message_color'))
-                if targeting:
-                    targeting_message = message
-                damage = item_choice['item_parameters'].get('damage')
-                radius = item_choice['item_parameters'].get('radius')
-                effect_range = item_choice['item_parameters'].get('range')
+        color = getattr(libtcod, item_choice['color'], libtcod.white)
+        item_component = None
+        equippable_component = None
+        targeting_message = None
+        treasure_value = item_choice.get('treasure_value', 0)
 
-                item_component = Item(use_function=use_function,
-                                      amount=amount,
-                                      is_treasure=is_treasure,
-                                      targeting=targeting,
-                                      targeting_message=targeting_message,
-                                      damage=damage,
-                                      radius=radius,
-                                      range=effect_range)
-            if item_choice.get('equippable'):
-                slot = slots.get(item_choice['equippable_parameters'].get('slot'))
-                power_bonus = item_choice['equippable_parameters'].get('power_bonus')
-                defense_bonus = item_choice['equippable_parameters'].get('defense_bonus')
-                equippable_component = Equippable(slot,
-                                                  power_bonus=power_bonus,
-                                                  defense_bonus=defense_bonus)
+        if item_choice.get('item'):
+            use_function = ItemFactory.use_functions.get(item_choice['item_parameters'].get('function'))
+            amount = item_choice['item_parameters'].get('amount')
+            targeting = item_choice['item_parameters'].get('targeting')
+            is_treasure = item_choice['item_parameters'].get('is_treasure', False)
+            message = item_choice['item_parameters'].get('message')
+            if message:
+                message = Message(message,
+                                  item_choice['item_parameters'].get('message_color'))
+            if targeting:
+                targeting_message = message
+            damage = item_choice['item_parameters'].get('damage')
+            radius = item_choice['item_parameters'].get('radius')
+            effect_range = item_choice['item_parameters'].get('range')
 
-            item = Entity(x, y, color=color,
-                          char=item_choice['symbol'],
-                          name=item_choice['name'],
-                          render_order=RenderOrder.ITEM,
-                          item=item_component,
-                          count_value=1,
-                          treasure_value=treasure_value,
-                          equippable=equippable_component)
-            return item
+            item_component = Item(use_function=use_function,
+                                  amount=amount,
+                                  is_treasure=is_treasure,
+                                  targeting=targeting,
+                                  targeting_message=targeting_message,
+                                  damage=damage,
+                                  radius=radius,
+                                  range=effect_range)
+        if item_choice.get('equippable'):
+            slot = ItemFactory.slots.get(item_choice['equippable_parameters'].get('slot'))
+            power_bonus = item_choice['equippable_parameters'].get('power_bonus')
+            defense_bonus = item_choice['equippable_parameters'].get('defense_bonus')
+            equippable_component = Equippable(slot,
+                                              power_bonus=power_bonus,
+                                              defense_bonus=defense_bonus)
+
+        item = Entity(x, y, color=color,
+                      char=item_choice['symbol'],
+                      name=item_choice['name'],
+                      render_order=RenderOrder.ITEM,
+                      item=item_component,
+                      count_value=1,
+                      treasure_value=treasure_value,
+                      equippable=equippable_component)
+        return item
+    
+    @staticmethod
+    def build_item(x, y, item_color='white', item_symbol='.', item_name='Generic Item',
+                   treasure_value=0,
+                   is_item=False, item_parameters=None,
+                   is_equippable=False, equippable_parameters=None):
+
+        color = getattr(libtcod, item_color, libtcod.white)
+        item_component = None
+        equippable_component = None
+        targeting_message = None
+        treasure_value = treasure_value
+
+        if is_item:
+            use_function = ItemFactory.use_functions.get(item_parameters.get('function'))
+            amount = item_parameters.get('amount')
+            targeting = item_parameters.get('targeting')
+            is_treasure = item_parameters.get('is_treasure', False)
+            message = item_parameters.get('message')
+            if message:
+                message = Message(message,
+                                  item_parameters.get('message_color'))
+            if targeting:
+                targeting_message = message
+            damage = item_parameters.get('damage')
+            radius = item_parameters.get('radius')
+            effect_range = item_parameters.get('range')
+
+            item_component = Item(use_function=use_function,
+                                  amount=amount,
+                                  is_treasure=is_treasure,
+                                  targeting=targeting,
+                                  targeting_message=targeting_message,
+                                  damage=damage,
+                                  radius=radius,
+                                  range=effect_range)
+        if is_equippable:
+            slot = ItemFactory.slots.get(equippable_parameters.get('slot'))
+            power_bonus = equippable_parameters.get('power_bonus')
+            defense_bonus = equippable_parameters.get('defense_bonus')
+            equippable_component = Equippable(slot,
+                                              power_bonus=power_bonus,
+                                              defense_bonus=defense_bonus)
+
+        item = Entity(x, y, color=color,
+                      char=item_symbol,
+                      name=item_name,
+                      render_order=RenderOrder.ITEM,
+                      item=item_component,
+                      count_value=1,
+                      treasure_value=treasure_value,
+                      equippable=equippable_component)
+        return item
--- map_objects/monster_factory.py
+++ map_objects/monster_factory.py
@@ -7,26 +7,53 @@ from render_functions import RenderOrder
 
 
 class MonsterFactory:
+    monster_ai = {'basic': BasicMonster,
+                  None: BasicMonster}
+
     @staticmethod
     def get_monster(monster_dict, monster_chances, entities, x, y):
-
         if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-            monster_choice = monster_dict[random_choice_from_dict(monster_chances)]
-            fighter_component = Fighter(hp=monster_choice['hp'],
-                                        defense=monster_choice['defense'],
-                                        power=monster_choice['power'],
-                                        xp=monster_choice['xp'])
-            color = getattr(libtcod, monster_choice['color'], libtcod.white)
-            if monster_choice['ai'] == 'basic':
-                ai_component = BasicMonster()
-            else:
-                ai_component = BasicMonster()
-            monster = Entity(x, y, monster_choice['symbol'],
-                             color,
-                             monster_choice['name'],
-                             count_value=monster_choice['monster_value'],
-                             treasure_value=monster_choice['treasure_value'],
-                             blocks=True, render_order=RenderOrder.ACTOR,
-                             fighter=fighter_component, ai=ai_component)
+            return MonsterFactory.get_monster_by_name(monster_dict,
+                                                      random_choice_from_dict(monster_chances),
+                                                      x, y)
 
-            return monster
+    @staticmethod
+    def get_monster_by_name(monster_dict, monster_name, x, y):
+        monster_choice = monster_dict[monster_name]
+        fighter_component = Fighter(hp=monster_choice['hp'],
+                                    defense=monster_choice['defense'],
+                                    power=monster_choice['power'],
+                                    xp=monster_choice['xp'])
+        color = getattr(libtcod, monster_choice['color'], libtcod.white)
+        ai_component = MonsterFactory.monster_ai[monster_choice.get('ai')]()
+        monster = Entity(x, y, monster_choice['symbol'],
+                         color,
+                         monster_choice['name'],
+                         count_value=monster_choice['monster_value'],
+                         treasure_value=monster_choice['treasure_value'],
+                         blocks=True, render_order=RenderOrder.ACTOR,
+                         fighter=fighter_component, ai=ai_component)
+
+        return monster
+
+    @staticmethod
+    def build_monster(x, y, monster_symbol='m', monster_color='white',
+                      monster_name='generic_monster', monster_ai='basic',
+                      fighter_hp=1, fighter_defense=0, fighter_power=0, fighter_xp=0,
+                      treasure_value=0, count_value=0):
+        fighter_component = Fighter(hp=fighter_hp,
+                                    defense=fighter_defense,
+                                    power=fighter_power,
+                                    xp=fighter_xp)
+        color = getattr(libtcod, monster_color, libtcod.white)
+        ai_component = MonsterFactory.monster_ai[monster_ai]()
+        monster = Entity(x, y, monster_symbol,
+                         color,
+                         monster_name,
+                         count_value=count_value,
+                         treasure_value=treasure_value,
+                         blocks=True, render_order=RenderOrder.ACTOR,
+                         fighter=fighter_component, ai=ai_component)
+
+        return monster
+
