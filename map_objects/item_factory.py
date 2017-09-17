0657a9373c71f50340edd4beb72f127cc605c05c
Code Dragon <Kehvarl@gmail.com>
2017-09-17 16:08:39 -0700

    Added docstrings to ITem and Monster Factories

--- map_objects/item_factory.py
+++ map_objects/item_factory.py
@@ -22,14 +22,31 @@ class ItemFactory:
 
     @staticmethod
     def get_item(item_dict, item_chances, entities, x, y):
+        """
+        Select a random level-appropriate item from the available items, and add it to the map
+        :param dictionary item_dict: Dictionary of all available pre-defined items
+        :param list item_chances: Item-generation probabilities for the current level
+        :param list entities: Items and Monsters already on the Map.
+        :param int x: X position on map
+        :param int y: Y position on map
+        :return Entity: Item to be placed in dungeon
+        """
         if not any([entity for entity in entities if entity.x == x and entity.y == y]):
             return ItemFactory.get_item_by_name(item_dict,
                                                 random_choice_from_dict(item_chances),
                                                 x, y)
 
     @staticmethod
-    def get_item_by_name(item_dict, item_name, x, y):
-        item_choice = item_dict[item_name]
+    def get_item_by_name(item_dict, item_key, x, y):
+        """
+
+        :param dictionary item_dict: Dictionary of all available pre-defined items
+        :param str item_key: Key Value to find Item from item_dict
+        :param int x: X position on map
+        :param int y: Y position on map
+        :return Entity: Item to be placed in dungeon
+        """
+        item_choice = item_dict[item_key]
 
         color = getattr(libtcod, item_choice['color'], libtcod.white)
         item_component = None
@@ -83,6 +100,20 @@ class ItemFactory:
                    treasure_value=0,
                    is_item=False, item_parameters=None,
                    is_equippable=False, equippable_parameters=None):
+        """
+        Create and set an Item at the specified coordinates
+        :param int x: X position on map
+        :param int y: Y position on map
+        :param str item_color: Color to use for Item icon
+        :param str item_symbol: Icon to display for the Item
+        :param str item_name: Display name for the Item
+        :param int treasure_value: Equivalent Treasure Value of the item
+        :param boolean is_item: True if the item is a Consumable
+        :param dictionary item_parameters:  Dictionary of Item Properties
+        :param boolean is_equippable: True if the Item can be equipped for bonuses
+        :param dictionary equippable_parameters: Dictionary of Equipment Properties
+        :return Entity: Item to be placed in dungeon
+        """
 
         color = getattr(libtcod, item_color, libtcod.white)
         item_component = None
--- map_objects/monster_factory.py
+++ map_objects/monster_factory.py
@@ -12,14 +12,31 @@ class MonsterFactory:
 
     @staticmethod
     def get_monster(monster_dict, monster_chances, entities, x, y):
+        """
+        Randomly Select a Monster from the available monsters, and set it in the map
+        :param dictionary monster_dict: dictionary of all available pre-defined monsters
+        :param list monster_chances: Monster-generation probabilities for the current level
+        :param list entities: Items and Monsters already on the Map.
+        :param int x: X position on map
+        :param int y: Y position on map
+        :return Entity: Monster to be placed in dungeon
+        """
         if not any([entity for entity in entities if entity.x == x and entity.y == y]):
             return MonsterFactory.get_monster_by_name(monster_dict,
                                                       random_choice_from_dict(monster_chances),
                                                       x, y)
 
     @staticmethod
-    def get_monster_by_name(monster_dict, monster_name, x, y):
-        monster_choice = monster_dict[monster_name]
+    def get_monster_by_name(monster_dict, monster_key, x, y):
+        """
+        Load a pre-defined Monster and set it at the listed coordinated
+        :param dictionary monster_dict: dictionary of all available pre-defined monsters
+        :param str monster_key:  Key Value to find Monster from monster_dict
+        :param int x: X position on map
+        :param int y: Y position on map
+        :return Entity: Monster to be placed in dungeon
+        """
+        monster_choice = monster_dict[monster_key]
         fighter_component = Fighter(hp=monster_choice['hp'],
                                     defense=monster_choice['defense'],
                                     power=monster_choice['power'],
@@ -41,6 +58,22 @@ class MonsterFactory:
                       monster_name='generic_monster', monster_ai='basic',
                       fighter_hp=1, fighter_defense=0, fighter_power=0, fighter_xp=0,
                       treasure_value=0, count_value=0):
+        """
+        Create and set a Monster at the specified coordinates
+        :param int x: X position on map
+        :param int y: Y position on map
+        :param str monster_symbol: Monster icon
+        :param str monster_color: Color for monster icon
+        :param str monster_name: Display Name for monster
+        :param str monster_ai: Default AI to use for this monster
+        :param int fighter_hp: Starting Hit Points
+        :param int fighter_defense: Defense Combat Value
+        :param int fighter_power: Attack Combat Value
+        :param int fighter_xp: Experience Points earned for defeating monster
+        :param int treasure_value: Treasure dropped by monster
+        :param int count_value: Monster's presence count for use in random generation
+        :return Entity: Monster to be placed in dungeon
+        """
         fighter_component = Fighter(hp=fighter_hp,
                                     defense=fighter_defense,
                                     power=fighter_power,
