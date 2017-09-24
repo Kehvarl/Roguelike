62eb2e261b3d3ee155297f7ceb76d02341b862ae
Code Dragon <Kehvarl@gmail.com>
2017-09-24 10:56:32 -0700

    Render Function cleanup.  Added Fog Of War debug constant. BUGS   Monster spawners spawn a monster every round   Monster spawners don't check for entity presence before spawning   Monster spawners don't respect room monster limits

--- render_functions.py
+++ render_functions.py
@@ -4,6 +4,8 @@ import libtcodpy as libtcod
 from game_states import GameStates
 from menus import character_screen, inventory_menu, level_up_menu
 
+DISABLE_FOG_OF_WAR = False
+
 
 class RenderOrder(Enum):
     STAIRS = 1
@@ -48,8 +50,11 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
 
 
 # noinspection PyShadowingNames
-def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height,
-               bar_width, panel_height, panel_y, mouse, colors, game_state):
+def render_all(con, panel, entities, player,
+               game_map, fov_map, fov_recompute,
+               message_log, screen_width, screen_height,
+               bar_width, panel_height, panel_y,
+               mouse, colors, game_state):
     """
     Draws all entities in the list
     :param con: The console to draw on
@@ -76,19 +81,26 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
                 visible = libtcod.map_is_in_fov(fov_map, x, y)
                 wall = game_map.tiles[x][y].block_sight
 
-                # TODO Remove debug
-                if visible or True:
+                if visible or DISABLE_FOG_OF_WAR:
                     if wall:
-                        libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
+                        libtcod.console_set_char_background(con, x, y,
+                                                            colors.get('light_wall'),
+                                                            libtcod.BKGND_SET)
                     else:
-                        libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+                        libtcod.console_set_char_background(con, x, y,
+                                                            colors.get('light_ground'),
+                                                            libtcod.BKGND_SET)
                     game_map.tiles[x][y].explored = True
 
                 elif game_map.tiles[x][y].explored:
                     if wall:
-                        libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+                        libtcod.console_set_char_background(con, x, y,
+                                                            colors.get('dark_wall'),
+                                                            libtcod.BKGND_SET)
                     else:
-                        libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+                        libtcod.console_set_char_background(con, x, y,
+                                                            colors.get('dark_ground'),
+                                                            libtcod.BKGND_SET)
 
     # Draw entities in the list
     entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
@@ -156,8 +168,8 @@ def draw_entity(con, entity, fov_map, game_map):
     :param fov_map: The map holding Field of View information
     :param game_map: The map holding game tiles
     """
-    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y) or (
-                entity.stairs and game_map.tiles[entity.x][entity.y].explored) or True:
+    if DISABLE_FOG_OF_WAR or libtcod.map_is_in_fov(fov_map, entity.x, entity.y) or (
+            entity.stairs and game_map.tiles[entity.x][entity.y].explored):
         libtcod.console_set_default_foreground(con, entity.color)
         libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
